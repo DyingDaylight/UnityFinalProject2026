44af85389ab7cfc0bb891452d5e3d7d52a2f6d42
Ekaterina Gorodetskaia <galeyevaev@gmail.com>
2026-03-08 16:13:40 +0200

    PlayerFlipWhileMoving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,25 @@ public class PlayerMovement : MonoBehaviour
     private float moveSpeed = 3f;
     
     private Rigidbody2D rb;
+    private Animator animator;
+    
     private Vector2 input;
     private Vector2 movement;
+    private Vector2 lastMoveDirection = Vector2.down;
     
     private Vector3 originalScale;
+    
+    private string currentAnimation;
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -26,23 +37,64 @@ public class PlayerMovement : MonoBehaviour
         
         movement = input.normalized; // do not move faster by diagonal
         
-        if (input.x != 0 || input.y != 0)
+        bool isMoving = input.sqrMagnitude > 0.01f;
+        
+        
+        if (isMoving)
         {
-            /*float x = horizontalValue < 0 ? originalScale.x * -1 : originalScale.x;
-            transform.localScale = new Vector3(
-                x,
-                originalScale.y,
-                originalScale.x);*/
-            
-            //Vector3 deltaX = Vector3.right * horizontalValue * moveSpeed * Time.deltaTime;
-            //Vector3 deltaY = Vector3.up * verticalValue * moveSpeed * Time.deltaTime;
-            //Vector3 newPosition = transform.position + deltaX + deltaY;
-            
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                lastMoveDirection = new Vector2(Mathf.Sign(input.x), 0f);
+            else
+                lastMoveDirection = new Vector2(0f, Mathf.Sign(input.y));
         }
+        
+        animator.SetBool("IsMoving", isMoving);
+        animator.SetFloat("LastMoveX", lastMoveDirection.x);
+        animator.SetFloat("LastMoveY", lastMoveDirection.y);
+        
+        UpdateDirection(isMoving);
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = movement * moveSpeed;
+    }
+
+    private void UpdateDirection(bool isMoving)
+    {
+        string nextAnimation;
+
+        if (lastMoveDirection.x > 0f)
+        {
+            transform.localScale = new Vector3(
+                originalScale.x * -1,
+                originalScale.y,
+                originalScale.x);
+            
+            nextAnimation = isMoving ? "WalkSide" : "IdleSide";
+        }
+        else if (lastMoveDirection.x < 0f)
+        {
+            transform.localScale = new Vector3(
+                originalScale.x,
+                originalScale.y,
+                originalScale.x);
+
+            nextAnimation = isMoving ? "WalkSide" : "IdleSide";
+        }
+        else if (lastMoveDirection.y > 0f)
+        {
+            nextAnimation = isMoving ? "WalkUp" : "IdleUp";
+        }
+        else
+        {
+            nextAnimation = isMoving ? "WalkDown" : "IdleDown";
+        }
+
+        if (currentAnimation == nextAnimation)
+            return;
+
+        animator.Play(nextAnimation);
+        currentAnimation = nextAnimation;
     }
 }
