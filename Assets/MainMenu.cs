6113d2eff1d8ca3b2bf630f15cf865663051d7e1
tokyoboy701 <breakdance9696@gmail.com>
2026-03-11 02:29:55 +0200

    звук сундука, мейнменю и музыка

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public partial class MainMenu : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    [SerializeField] private string firstSceneName = "HubScene";
+    [SerializeField] private string startingSpawnPoint = "StartGamePoint";
+
+    public void PlayGame()
+    {
+        if (SceneTransferManager.Instance != null)
+        {
+            SceneTransferManager.Instance.librarySpawn = startingSpawnPoint;
+        }
+
+        SceneManager.LoadScene(firstSceneName);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Game Exited");
+        Application.Quit(); 
+    }
+}
